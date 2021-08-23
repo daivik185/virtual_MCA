a241f027397f61602dbd9197b89caed0dbdde03b
daivik185 <87440620+daivik185@users.noreply.github.com>
2021-08-24 00:18:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// window.addEventListener("scroll", function() {showFunction()});
+
+// function showFunction() {
+//     if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+//         document.getElementById("everything-at-once").style.display = "block";
+//     } else {
+//         document.getElementById("everything-at-once").style.display = "none";
+//     }
+// }
